9d5d57a1e4e49f4a0064108b91803efac5837c4e
The1337Gh0st <cwarrior1414@gmail.com>
2022-08-10 16:39:38 -0400

    Healing amplification nerf
    Nerfed healing amplification per level to 6% (capping at 150%)

--- lua/horde/gamemode/perks/reverend_base.lua
+++ lua/horde/gamemode/perks/reverend_base.lua
@@ -3,7 +3,7 @@ PERK.Description = [[
 The Reverend subclass is a hybrid subclass that can provide support for teammates or utilize high damage skills.
 Complexity: MEDIUM
 
-{6} increased healing. ({2} + {7} per level, up to {8})
+{6} healing amplification. ({2} + {7} per level, up to {8})
 {1} increased overheal. ({2} + {3} per level, up to {4})
 Kills will heal you and nearby players for {5} health. ]]
 
@@ -15,9 +15,9 @@ PERK.Params = {
     [3] = {value = 0.01, percent = true},
     [4] = {value = 0.25, percent = true},
 	[5] = {value = 2},
-	[6] = {percent = true, base = 0, level = 0.1, max = 2.5, classname = "Reverend"},
-	[7] = {value = 0.1, percent = true},
-	[8] = {value = 2.5, percent = true},
+	[6] = {percent = true, base = 0, level = 0.06, max = 1.5, classname = "Reverend"},
+	[7] = {value = 0.06, percent = true},
+	[8] = {value = 1.5, percent = true},
 }
 
 PERK.Hooks = {}
@@ -37,7 +37,7 @@ PERK.Hooks.Horde_OnPlayerHeal = function(ply, healinfo)
     local healer = healinfo:GetHealer()
 	local r = healer:Horde_GetPerkLevelBonus("reverend_base")
     if healer:IsPlayer() and healer:Horde_GetPerk("reverend_base") then
-        healinfo:SetHealAmount(healinfo:GetHealAmount() * ((r * 10) + 1))
+        healinfo:SetHealAmount(healinfo:GetHealAmount() * ((r * 6) + 1))
 		healinfo:SetOverHealPercentage(r)
     end
 end
